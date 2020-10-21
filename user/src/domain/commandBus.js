03f44cf8279234f4b4e57837bc71edadcee76777
Stefanos Psarras <stepsarras@gmail.com>
2020-10-21 16:29:09 +0300

    add return at catch

--- user/src/domain/commandBus.js
+++ user/src/domain/commandBus.js
@@ -19,74 +19,75 @@ import logger from '../logger';
  * (in hexagonal architecture lingo).
  */
 class CommandBus {
-	/**
-	 * Create a new CommandBus instance
-	 *
-	 * @param {Object} options
-	 * @param {Registry} options.registry - a registry of dependencies
-	 * to inject to resolved handlers.
-	 */
-	constructor({registry}) {
-		this.registry = registry;
-	}
+  /**
+   * Create a new CommandBus instance
+   *
+   * @param {Object} options
+   * @param {Registry} options.registry - a registry of dependencies
+   * to inject to resolved handlers.
+   */
+  constructor({registry}) {
+    this.registry = registry;
+  }
 
-	/**
-	 * Execute the given command using its designated hander,
-	 * and return a Promise.
-	 *
-	 * @param {Object} command - the command to execute
-	 * @returns {Promise} a promise that resolves if the command
-	 * executes successfully and rejects if it fails
-	 * @throws if no handler is found for the given command
-	 */
-	async execute(command) {
-		if (!command) {
-			throw new Error('CommandBus: execute expects a command');
-		}
-		// console.log('command.constructor.name', command.constructor.name);
-		let commandHandler;
-		switch (command.constructor.name) {
-			case 'CreateUserCommand':
-				commandHandler = new CreateUserHandler({
-					eventBus: this.registry.eventBus,
-					dB: this.registry.dB
-				});
-				break;
-			case 'GetUserCommand':
-				commandHandler = new GetUserHandler({
-					eventBus: this.registry.eventBus,
-					dB: this.registry.dB
-				});
-				break;
-			case 'UpdateUserCommand':
-				commandHandler = new UpdateUserHandler({
-					eventBus: this.registry.eventBus,
-					dB: this.registry.dB
-				});
-				break;
-			case 'DeleteUserCommand':
-				commandHandler = new DeleteUserHandler({
-					eventBus: this.registry.eventBus,
-					dB: this.registry.dB
-				});
-				break;
-			case 'CheckUserPassCommand':
-				commandHandler = new CheckUserPassCommandHandler({
-					eventBus: this.registry.eventBus,
-					dB: this.registry.dB
-				});
-				break;
-			default:
-				logger.error(`CommandBus: No handler found for command "${command.constructor.name}"`);
-				throw new Error(`CommandBus: No handler found for command "${command.constructor.name}"`);
-		}
+  /**
+   * Execute the given command using its designated hander,
+   * and return a Promise.
+   *
+   * @param {Object} command - the command to execute
+   * @returns {Promise} a promise that resolves if the command
+   * executes successfully and rejects if it fails
+   * @throws if no handler is found for the given command
+   */
+  async execute(command) {
+    if (!command) {
+      throw new Error('CommandBus: execute expects a command');
+    }
+    // console.log('command.constructor.name', command.constructor.name);
+    let commandHandler;
+    switch (command.constructor.name) {
+      case 'CreateUserCommand':
+        commandHandler = new CreateUserHandler({
+          eventBus: this.registry.eventBus,
+          dB: this.registry.dB
+        });
+        break;
+      case 'GetUserCommand':
+        commandHandler = new GetUserHandler({
+          eventBus: this.registry.eventBus,
+          dB: this.registry.dB
+        });
+        break;
+      case 'UpdateUserCommand':
+        commandHandler = new UpdateUserHandler({
+          eventBus: this.registry.eventBus,
+          dB: this.registry.dB
+        });
+        break;
+      case 'DeleteUserCommand':
+        commandHandler = new DeleteUserHandler({
+          eventBus: this.registry.eventBus,
+          dB: this.registry.dB
+        });
+        break;
+      case 'CheckUserPassCommand':
+        commandHandler = new CheckUserPassCommandHandler({
+          eventBus: this.registry.eventBus,
+          dB: this.registry.dB
+        });
+        break;
+      default:
+        logger.error(`CommandBus: No handler found for command "${command.constructor.name}"`);
+        throw new Error(`CommandBus: No handler found for command "${command.constructor.name}"`);
+    }
 
-		try {
-			return await commandHandler.handle(command);
-		} catch (error) {
-			logger.error(error);
-		}
-	}
+    try {
+      return await commandHandler.handle(command);
+    } catch (error) {
+      logger.error(error);
+      throw error;
+    }
+  }
 }
 
 export default CommandBus;
