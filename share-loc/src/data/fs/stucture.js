6296da9a119cba88775dd9139822054268f4e0ca
Stefanos Psarras <stepsarras@gmail.com>
2020-10-21 12:36:47 +0300

    fix(dataStructureDir): join the path with __dirname

--- share-loc/src/data/fs/stucture.js
+++ share-loc/src/data/fs/stucture.js
@@ -1,10 +1,18 @@
 import fs from 'fs-extra';
 import path from 'path';
 
-async function dataStuctureDir() {
+import logger from '../../logger';
+
+async function dataStructureDir() {
 	const dir = process.env.DATA_FS_DIR || '../../files/structure';
-	await fs.ensureDir(dir);
-	return dir;
+	try {
+		const dirToEnsure = path.join(__dirname, dir);
+		await fs.ensureDir(dirToEnsure);
+		return dir;
+	} catch (error) {
+		logger.error(`Error: dataStructureDir ${error.message}`);
+		throw error;
+	}
 }
 
 function filePath(dataDir, key) {
@@ -13,58 +21,57 @@ function filePath(dataDir, key) {
 
 async function readJSON(dataDir, key) {
 	const readFrom = filePath(dataDir, key);
-	// console.log(readFrom);
 	const data = await fs.readFile(readFrom, 'utf8');
 	return JSON.parse(data);
 }
 
-async function crupdate(key, json) {
-	const dataDir = await dataStuctureDir();
-	if (key.indexOf('/') >= 0) throw new Error(`key ${key} cannot contain '/'`);
-	//   var note = new Note(key, title, body);
-	const writeTo = filePath(dataDir, key);
-	const writeJSON = json;
+// async function crupdate(key, json) {
+//   const dataDir = await dataStructureDir();
+//   if (key.indexOf('/') >= 0) throw new Error(`key ${key} cannot contain '/'`);
+//   //   var note = new Note(key, title, body);
+//   const writeTo = filePath(dataDir, key);
+//   const writeJSON = json;
 
-	await fs.writeFile(writeTo, writeJSON, 'utf8');
-	return json;
-}
+//   await fs.writeFile(writeTo, writeJSON, 'utf8');
+//   return json;
+// }
 
-export function create(key, title, body) {
-	return crupdate(key, title, body);
-}
+// export function create(key, title, body) {
+// 	return crupdate(key, title, body);
+// }
 
-export function update(key, title, body) {
-	return crupdate(key, title, body);
-}
+// export function update(key, title, body) {
+// 	return crupdate(key, title, body);
+// }
 
 export async function read(key) {
-	const dataDir = await dataStuctureDir();
+	const dataDir = await dataStructureDir();
 	const structure = await readJSON(dataDir, key);
 
 	return structure;
 }
 
-export async function destroy(key) {
-	const dataDir = await dataStuctureDir();
-	await fs.unlink(filePath(dataDir, key));
-}
+// export async function destroy(key) {
+//   const dataDir = await dataStructureDir();
+//   await fs.unlink(filePath(dataDir, key));
+// }
 
-export async function keylist() {
-	const dataDir = await dataStuctureDir();
-	let filez = await fs.readdir(dataDir);
-	if (!filez || typeof filez === 'undefined') filez = [];
+// export async function keylist() {
+//   const dataDir = await dataStructureDir();
+//   let filez = await fs.readdir(dataDir);
+//   if (!filez || typeof filez === 'undefined') filez = [];
 
-	const thenotes = filez.map(async fname => {
-		const key = path.basename(fname, '.json');
+//   const thenotes = filez.map(async fname => {
+//     const key = path.basename(fname, '.json');
 
-		const rec = await readJSON(dataDir, key);
-		return rec.key;
-	});
-	return Promise.all(thenotes);
-}
+//     const rec = await readJSON(dataDir, key);
+//     return rec.key;
+//   });
+//   return Promise.all(thenotes);
+// }
 
 export async function count() {
-	const dataDir = await dataStuctureDir();
+	const dataDir = await dataStructureDir();
 	const filez = await fs.readdir(dataDir);
 	return filez.length;
 }
