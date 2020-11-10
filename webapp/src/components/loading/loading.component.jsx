8a7b63905779d1408d9b50156ceb0bdb3a856435
Stefanos Psarras <stepsarras@gmail.com>
2020-11-10 21:17:56 +0200

    feat: add react-spinners

--- webapp/src/components/loading/loading.component.jsx
+++ webapp/src/components/loading/loading.component.jsx
@@ -1,13 +1,19 @@
 import React from "react"; 
-import { Container } from "react-bootstrap";
+import { FadeLoader } from "react-spinners";
 
 const Loading = () => {
- 
 	return (
-		<Container fluid>
-		<div>Loading...</div>
-		</Container>
-	);
+		<div style={{
+			position: 'absolute',
+			top: `calc(50% - 75 )`,
+			left: `calc(50% - 75 )`
+		}}><FadeLoader  color={"lightblue"} size={150} /></div>
+		);
+	// return (
+	// 	<Container fluid>
+	// 	<div>Loading...</div>
+	// 	</Container>
+	// );
 };
 
 export default Loading;
