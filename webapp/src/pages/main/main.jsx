5e89fdfce6d08ad41079ab3528fbe5f696648a0c
Stefanos Psarras <stepsarras@gmail.com>
2020-11-16 18:46:25 +0200

    fixup! feat: add functions getBackEndHost & getBackEndWsHost

--- webapp/src/pages/main/main.jsx
+++ webapp/src/pages/main/main.jsx
@@ -1,21 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import { Container, Row } from "react-bootstrap";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Container, Row } from 'react-bootstrap';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import { useAuth } from "../../components/auth/auth.component";
-import { ProvideShareLoc } from "../../components/share-loc/share-loc.component"
+import { useAuth } from '../../components/auth/auth.component';
+import { ProvideShareLoc } from '../../components/share-loc/share-loc.component';
 
-import Header from "../../components/header/header.component";
-import SlideBar from "../../components/slide-bar/slide-bar.component";
+import Header from '../../components/header/header.component';
+import SlideBar from '../../components/slide-bar/slide-bar.component';
 
-import SignInPage from "../sign-in/sign-in";
-import { routes } from "./routes.data";
- 
+import SignInPage from '../sign-in/sign-in';
+import { routes } from './routes.data';
 
 const getRoutes = (key, routes, href) => {
 	if (!href) {
-		href = "";
+		href = '';
 	}
 	if (routes.path) {
 		href += routes.path;
@@ -24,7 +23,7 @@ const getRoutes = (key, routes, href) => {
 	let retArr = [];
 
 	if (!key) {
-		key = "route";
+		key = 'route';
 	}
 
 	if (routes.isLink) {
@@ -35,23 +34,22 @@ const getRoutes = (key, routes, href) => {
 					key={`redirect-${key}`}
 					from="/"
 					to={`${href}`}
-					component={(props) => {
+					component={props => {
 						return <routes.component {...props} />;
 					}}
 				/>
-			); 
-		}else{
+			);
+		} else {
 			retArr.push(
 				<Route
 					key={key}
 					path={`${href}`}
-					render={(props) => {
+					render={props => {
 						return <routes.component {...props} />;
 					}}
 				/>
 			);
 		}
-
 	}
 	if (routes.nodes) {
 		for (const property in routes.nodes) {
@@ -62,21 +60,22 @@ const getRoutes = (key, routes, href) => {
 	return retArr;
 };
 
-const MainPage = (props) => {
+const MainPage = props => {
 	const auth = useAuth();
- 
+
 	if (!auth.user) {
 		return <SignInPage />;
 	}
 
-	return ( 
+	return (
 		<ProvideShareLoc>
 			<Container fluid>
 				<Row>
 					<SlideBar items={routes} location={props.location.pathname} />
 					<main className="">
+					
 						<Header signOut={auth.signOut} userObj={auth.user} />
-						<Switch>{getRoutes("", routes, "")}</Switch>
+						<Switch>{getRoutes('', routes, '')}</Switch> 
 					</main>
 				</Row>
 			</Container>
