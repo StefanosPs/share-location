973f82b11f325425bb6a2dfa5f1ed3210e4f0cdc
Stefanos Psarras <stepsarras@gmail.com>
2020-11-17 12:55:36 +0200

    fixup! feat: add basic error handler

--- webapp/src/components/display-error/display-error.component.jsx
+++ webapp/src/components/display-error/display-error.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Alert } from 'react-bootstrap';
 
-const DisplayError = ({ children, ...props }) => {
-    <Alert variant="danger" dismissible>
-        {children}
-    </Alert>
+const DisplayError = ({ children }) => {
+	return (
+		<Alert variant="danger" dismissible>
+			{children}
+		</Alert>
+	);
 };
 
 export default DisplayError;
